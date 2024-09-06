c72cc828ee33b242830a5de4a7e2a3632c0e183b
Gabriel Pereira <144967056+gabrielpereirapb@users.noreply.github.com>
2024-09-06 14:20:43 -0300

    Delete components/relatarproblema/relatarproblema.js

--- components/relatarproblema/relatarproblema.js
+++
@@ -1,9 +0,0 @@
-function confirmar() {
-    // Aqui você pode adicionar a lógica para salvar as informações
-    // Exemplo: Enviar uma requisição AJAX para salvar os dados
-
-    // Após salvar, redirecionar para a página inicial com uma mensagem
-    const url = new URL('home.html', window.location.href);
-    url.searchParams.append('mensagem', 'Operação bem-sucedida!');
-    window.location.href = url;
-}
