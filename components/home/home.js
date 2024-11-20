6a6d8e3b43c4129fea3b440697010455234ddff2
vitormnegreiros <145074220+vitormnegreiros@users.noreply.github.com>
2024-11-19 22:19:36 -0300

    Update home.js

--- components/home/home.js
+++ components/home/home.js
@@ -1,1 +1,44 @@
+import validarToken from "../auth/auth.js";
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const valido = validarToken();
+
+    console.log(valido)
+
+
+    if(!valido){
+        window.location.href = "/components/exceptions/NaoAutorizadoException.html";
+
+    }
+
+});
+
+const fetchData = async () => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+        console.error('Token não encontrado.');
+        return;
+    }
+
+    try {
+        const response = await fetch('http://localhost:3000/api/protegerota', {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${localStorage.getItem('token')}`  // Adiciona o token no header Authorization
+            }
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            console.log('Dados:', data);
+        } else {
+            console.error('Erro na autenticação:', response.statusText);
+        }
+    } catch (error) {
+        console.error('Erro ao fazer a requisição:', error);
+    }
+};
 
